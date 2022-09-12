2272b53aa47bc32834e8dfc037a63bbf6cb043f2
prateekvarma <prtk.varma@gmail.com>
2022-09-11 23:35:58 -0600

    added tailwind css styling to create page

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Todos</title>
 </head>
 <body>
-    <form action="/todos/create" method="post">
-        @csrf
-        <input type="text" name="title" />
-        <input type="submit" value="create" />
-    </form>
+    <div class="text-center pt-10">
+        <h1 class="text-2xl">Add a todo item</h1>
+        <form action="/todos/create" method="post" class="py-5">
+            @csrf
+            <input type="text" name="title" class="py-2 px-2 border rounded" />
+            <input type="submit" value="create" class="p-2 border rounded" />
+        </form>
+    </div>
 </body>
 </html>
