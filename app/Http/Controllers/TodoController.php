392247636147860762ef2c391dbd68c30b973130
prateekvarma <prtk.varma@gmail.com>
2022-09-14 23:30:26 -0600

    Created validation to create, and added custom messages - using Validator::make

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TodoController extends Controller
 {
@@ -24,6 +25,20 @@ class TodoController extends Controller
 
     public function store(Request $request)
     {
+        $rules = [
+            'title' => 'required|max:255',
+        ];
+
+        $messages = [
+            'title.max' => 'Title way too long!',
+            'title.required' => 'You need to write something!',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+        
         Todo::create($request->all());
         return redirect()->back()->with('message', 'Todo created successfully!');
     }
--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -5,3 +5,15 @@
         <div class="py-4 px-2 bg-red-400">{{ session()->get('erro') }}</div>
     @endif
 </div>
+
+<!-- Below for validation errors -->
+
+@if ($errors->any())
+    <div class="py-4 px-2 bg-red-400">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
