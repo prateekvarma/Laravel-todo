f69d14dd6460c7b949399bff67f07e0cb1722a02
prateekvarma <prtk.varma@gmail.com>
2022-10-09 22:22:17 -0600

    Add new todo button

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,9 +1,11 @@
 @extends('todos.layout')
 
 @section('content')
-
-<h1 class="text-2xl">All Todos: </h1>
-    <ul>
+<div class="flex justify-center">
+    <h1 class="text-2xl">All Todos: </h1>
+    <a href="/todos/create" class="mx-5 py-1 px-1 bg-blue-400 cursor-pointer rounded text-white">New Todo</a>
+</div>
+    <ul class="my-5">
     @foreach($todos as $todo)
         <li>
             {{ $todo->title }}
